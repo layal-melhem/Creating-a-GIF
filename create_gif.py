f9bcac6d510923ea40a694ecae5b2fff51c655a1
Layal Melhem <185387254+layal-melhem@users.noreply.github.com>
2026-05-25 16:28:31 +0400

    Stylistic update

--- create_gif.py
+++ create_gif.py
@@ -1,7 +1,7 @@
 import imageio.v3 as iio
 
 filenames = ['nyan-cat1.png', 'nyan-cat2.png', 'nyan-cat3.png']
-images = [ ]
+images = []
 
 for filename in filenames:
   images.append(iio.imread(filename))
